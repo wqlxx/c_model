00a0e6e7aaf6f9fe362de74698831fa67766b335
wqlxx <wqlxx@yahoo.com.cn>
2013-04-16 15:01:27 +0800

    Update tcam_write.c

--- tcam_write.c
+++ tcam_write.c
@@ -511,3 +511,507 @@ _sys_humber_acl_get_block_index(sys_acl_block_t *pb, uint32 after_entry_id, uint
 	*block_index = target_idx;
 	return CTC_E_NONE;
 }	
+
+static sys_aclqos_entry_ctl_t sys_aclqos_entry_ctl;
+static ctc_hash_t *p_sys_acl_redirect_hash[CTC_MAX_LOCAL_CHIP_NUM];
+static sys_aclqos_entry_ctl_t *acl_master = &sys_aclqos_entry_ctl;
+
+void*
+ctc_hash_lookup(ctc_hash_t* hash, void *data)
+{
+	uint32 key = 0;
+	uint32 index = 0;
+	uint32 hash_size = 0;
+	uint16 idx_1d = 0;
+	uint16 idx_2d = 0;
+	ctc_hash_backet_t *backet = NULL;
+	
+	if(!hash)
+		return NULL;
+	
+	hash_size = hash->block_size * hash->block_num;
+	key = (*hash->hash_key)(data);
+	index = key % hash_size;
+
+	idx_1d = index / hash->block_size;
+	idx_2d = index % hash->block_size;
+
+	if(!hash->index[idx_1d])
+	{
+		return NULL;
+	}
+
+	for(backet = hash->index[idx_1d][idx_2d]; backet != NULL; backet = backet->next)
+	{
+		if(backet->key == key && (*hash->hash_cmp)(backet->data, data) == TRUE_
+		{
+			return backet->data;
+		}
+	}
+
+	return NULL;
+}
+
+int32
+sys_humber_aclqos_label_lookup(uint32 lable_id, uint8 is_service_label, sys_aclqos_label_t** pp_label)
+{
+	sys_aclqos_label_t label;
+	
+	CTC_NOT_ZERO_CHECK(label_id);
+	CTC_PTR_VALID_CHECK(pp_label);
+	
+	SYS_ACLQOS_LABEL_DBG_FUNC();
+	
+	label.id = label_id;
+		
+	if(is_service_label)
+	{
+		*pp_label = ctc_hash_lookup(p_service_label_hash, &label);
+	}else{
+		*pp_label = ctc_hash_lookup(p_aclqos_label_hash, &label);	
+	}
+	
+	return CTC_E_NONE;
+}
+	
+struct sys_chip_master_s
+{
+	uint8 lchip_num;
+	uint8 resv;
+	uint8 g_chip_id[CTC_MAX_LOCAL_CHIP_NUM];
+}
+typedef struct sys_chip_master_s sys_chip_master_t;
+
+static sys_chip_master_t *p_chip_master = NULL;
+
+uint8
+sys_humber_get_local_chip_num(void)
+{
+	if( NULL == p_chip_master)
+		return 0;
+	return p_chip_master->lchip_num;
+}
+
+static int32
+_sys_humber_aclqos_map_action(uint8 lchip, sys_aclqos_label_t* p_label, ctc_aclqos_action_t *p_ctc_action, sys_aclqos_action_t *p_sys_action)
+{
+	ctc_direction_t dir;
+	sys_nh_offset_array_t offset_array;
+	sys_humber_opf_t opf;
+	uint32 ds_fwd_offset = 0;
+	uint32 ds_fwd_base = 0;
+	sys_nh_info_ecmp_t* p_nhinfo = 0;
+	sys_acl_redirect_t acl_redirect;
+	sys_acl_redirect_t *p_acl_redirect = 0;
+	uint32 ret = 0;
+
+	CTC_PTR_VALID_CHECK(pp_label);
+	CTC_PTR_VALID_CHECK(p_ctc_action);
+	CTC_PTR_VALID_CHECK(p_sys_action);
+
+	SYS_ACLQOS_ENTRY_DBG_FUNC();
+	
+	kal_memset(&opf, 0, sizeof(opf));
+	kal_memset(&ad_redirect, 0, sizeof(acl_redirect));
+
+	dir = p_label->dir;
+	
+	/*deny*/
+	if(p_ctc_action->flag & CTC_ACLQOS_ACTION_DISCARD_FLAG)
+	{
+		p_sys_action->flag.discard = 1;
+	}
+	
+	//deny bridging
+	if(p_ctc_action->flag & CTC_ACLQOS_ACTION_DENY_BRIDGE_FLAG)
+	{	
+		p_sys_action->flag.deny_bridge = 1;		
+	}
+
+	//deny learning
+	if(p_ctc_action->flag & CTC_ACLQOS_ACTION_DENY_LEARN_FLAG)
+	{	
+		p_sys_action->flag.deny_learning = 1;		
+	}
+
+	//deny route
+	if(p_ctc_action->flag & CTC_ACLQOS_ACTION_DENY_ROUTE_FLAG)
+	{	
+		p_sys_action->flag.deny_route = 1;		
+	}
+
+	//deny replace cos
+	if(p_ctc_action->flag & CTC_ACLQOS_ACTION_DENY_REPLACE_COS_FLAG)
+	{	
+		p_sys_action->flag.deny_replace_cos = 1;		
+	}
+
+	//deny replace dscp
+	if(p_ctc_action->flag & CTC_ACLQOS_ACTION_DENY_REPLACE_DSCP_FLAG)
+	{	
+		p_sys_action->flag.deny_replace_dscp = 1;		
+	}
+
+	//stats
+	if(p_ctc_action->flag & CTC_ACLQOS_ACTION_STATS_FLAG)
+	{	
+		if(p_ctc_action->flags & CTC_ACLQOS_ACTION_FLOW_ID_FLAGS)
+		{
+			return CTC_E_ACLQOS_COLLISION_FIELD;
+		}
+		
+		p_sys_action->flag.stats = 1;
+		p_sys_action->stats_or_flowid.stats_ptr = SPECIAL_STATS_PTR;
+	
+		if(p_ctc_action->stats_ptr >= 4096)
+		{
+			return CTC_E_INVALID_FARAM;
+		}
+		p_sys_action->stats_or_flowid.stats_ptr = p_ctc_action->stats_ptr;
+		SYS_ACLQOS_ENTRY_DBG_INFO();	
+	}
+	
+	//flow id
+	if(p_ctc_action->flag & CTC_ACLQOS_ACTION_FLOW_ID_FLAG)
+	{	
+		CTC_MAX_VALUE_CHECK(p_ctc_action->flow_id, 254);
+		p_sys_action->flag.flow_id = 1;		
+		
+		p_sys_action->stats_or_flowid.flow_id = 255 - p_ctc_action->flow_id;
+	}
+
+	//flow policer
+	if(p_ctc_action->flag & CTC_ACLQOS_ACTION_FLOW_POLICER_FLAG)
+	{	
+		p_sys_action->flag.flow_policer = 1;	
+		p_sys_action->policer_id = p_ctc_action->policer_id;
+	}
+
+	//copy to cpu
+	if(p_ctc_action->flag & CTC_ACLQOS_ACTION_COPY_TO_CPU_FLAG)
+	{	
+		CTC_MAX_VALUE_CHECK(p_ctc_action->log_weight, 0xF);
+		p_ctc_action->log_weight = p_ctc_action->log_weight ? p_ctc_action->log_weight : OxF;
+
+		p_sys_action->flag.copy_to_cpu = 1;
+		p_sys_action->acl_log_id = CTC_ACLQOS_LOG_SESSION_3;
+		p_sys_action->random_threshold_shift = p_ctc_action->log_weight;
+	
+	}
+
+	//invalid
+	if(p_ctc_action->flag & CTC_ACLQOS_ACTION_INVALID_FLAG)
+	{	
+		p_sys_action->flag.invalid = 1;		
+	}
+
+	return CTC_E_NONE;
+}
+
+
+static int32
+_sys_humber_aclqos_map_key(uint8 lchip, sys_aclqos_label_t *p_label, ctc_aclqos_key_t *p_ctc_key, sys_aclqos_key_t *p_sys_key)
+{
+	sys_aclqos_label_index_t *p_label_index;
+	
+
+	CTC_PTR_VALID_CHECK(pp_label);
+	CTC_PTR_VALID_CHECK(p_ctc_key);
+	CTC_PTR_VALID_CHECK(p_sys_key);
+
+	p_label_index = p_label->p_index[lchip];
+	CTC_PTR_VALID_CHECK(p_label_index);
+	
+	p_sys_key->type = p_ctc_key->type;
+	
+	if(SYS_PBR_ACL_LABEL != p_label->tyoe)
+	{
+		switch(p_sys_key->type)
+		{
+			case: CTC_ACLQOS_MAC_KEY:
+				CTC_ERROR_RETURN(
+					_sys_humber_aclqos_map_mac_key(lchip, p_label, &p_ctc_key->key_info.mac_key, &p_sys_key->key_info.mac_key));
+				break;
+			case: CTC_ACLQOS_IPV4_KEY:
+				CTC_ERROR_RETURN(
+					_sys_humber_aclqos_map_ipv4_key(lchip, p_label, &p_ctc_key->key_info.ipv4_key, &p_sys_key->key_info.ipv4_key));
+				break;	
+			case: CTC_ACLQOS_MPLS_KEY:
+				CTC_ERROR_RETURN(
+					_sys_humber_aclqos_map_mpls_key(lchip, p_label, &p_ctc_key->key_info.mpls_key, &p_sys_key->key_info.mpls_key));
+				break;	
+			case: CTC_ACLQOS_IPV6_KEY:
+				CTC_ERROR_RETURN(
+					_sys_humber_aclqos_map_ipv6_key(lchip, p_label, &p_ctc_key->key_info.ipv6_key, &p_sys_key->key_info.ipv6_key));
+				break;	
+			default:
+				return CTC_E_ACLQOS_INVALID_KEY_TYPE;
+		}
+	}else{
+		/*NULL*/
+		return CTC_E_ACLQOS_INVALID_KEY_TYPE;
+	}
+	
+	return CTC_E_NONE;
+}
+
+void
+ctc_list_pointer_insert_tail(ctc_list_pointer_t *p_list, ctc_list_pointer_node_t *p_node)
+{
+	p_node->p_next = NULL;
+	p_node->p_prev = _CTC_LTAIL(p_list);
+	
+}
+
+
+static int32
+_sys_humber_aclqos_add_entry_to_db(sys_acl_block_t *pb, sys_aclqos_entry_t *p_entry)
+{
+	sys_aclqos_label_index_t *p_index = NULL;
+	ctc_list_pointer_t *p_list = NULL;
+	sys_aclqos_label_t *p_label;
+	
+	p_label = p_entry->p_label;
+	p_index = p_label->p_index[0];
+	p_list = &p_index->entry_list[p_entry->key.type];
+	ctc_list_pointer_insert_tail(p_list, &p_entry->head);
+	
+	//add to hash
+	ctc_hash_insert(acl_master->entry, p_entry);
+	//add to block
+	pb->entries[p_entry->block_index] = p_entry;
+	
+	//free count
+	//ignore the default entry
+	if(p_entry->block_index < pb->entry_count)
+	{
+		(pb->free_count)--;
+	}
+
+	return CTC_E_NONE;
+}
+
+static int32
+_sys_humber_aclqos_remove_entry_from_db(sys_acl_block_t *pb, sys_aclqos_entry_t *p_entry)
+{
+	sys_aclqos_label_index_t *p_index = NULL;
+	ctc_list_pointer_t *p_list = NULL;
+	sys_aclqos_label_t *p_label;
+	sys_humber_opf_t opf;
+	uint32 ds_fwd_offset = 0;
+	sys_acl_redirect_t acl_redirect;
+	sys_acl_redirect_t *p_acL_redirect = 0;
+	
+	p_label = p_entry->p_label;
+	p_index = p_label->p_index[0];
+	p_list  = &p_index->entry_list[p_entry->key.type];
+	
+	//unbind flow policer
+	if(p_entry->action.flag.flow_policer)
+	{
+		sys_humber_qos_flow_policer_unbind(p_sys_acl_redirect_hash[pb->lchip], &acl_redirect);
+		if(p_acl_redirect)
+		{
+			p_acl_redirect->ref--;
+		
+			if(p_acl_redirect->ref == 0)
+			{
+				opf.pool_type = OPF_ACL_FWD_SRAM;
+				opf.pool_index = pb->lchip;
+				ds_fwd_offset = p_entry->action.ds_fwd_ptr;
+				CTC_ERROR_RETURN(sys_humber_opf_free_offset9&opf, 1, ds_fwd_offset));
+	
+				ctc_hash_remove(p_sys_acl_redirect_hash[pb->lchip], p_acl_redirect);
+				mem_free(p_acl_redirect);
+			}
+		}else{
+			return CTC_E_ACLQOS_INVALID_ACTION;
+		}
+	}
+
+	//remove from hash
+	ctc_hash_remove(acl_master->entry, p_entry);
+	
+	//removr from block
+	pb->entries[p_entry->block_index] = NULL;
+	
+	//free count++
+	//ignore the default entry
+	if(p_entry->block_index < pb->entry_count)
+	{
+		(pb->free_count)++;
+	}
+		
+	if(p_entry->head.p_prev)
+	{
+		ctc_list_pointer_delete(p_list, &p_entry->head);
+	}
+	mem_free(p_entry);
+		
+	return CTC_E_NONE;
+}
+
+static int32
+_sys_humber_aclqos_entry_write(sys_acl_block_t *pb, sys_aclqos_entry_t *p_entry)
+{
+	sys_aclqos_sub_entry_info_t info;
+	
+	CTC_PTR_VALID_CHECK(p_entry);
+	
+	memset(&info, 0, sizeof(info));
+	info.offset = p_entry->block_index;
+	
+	CTC_ERROR_RETURN(
+		_sys_humber_aclqos_write_entry_to_chip(0, p_entry->p_label, p_entry, &info));
+	
+	//add to database
+	CTC_ERROR_RETURN(
+		_sys_humber_aclqos_add_entry_to_db(pb, p_entry));
+	
+	return CTC_E_NONE;
+}
+
+static int32
+_sys_humber_aclqos_entry_remove(sys_acl_block_t *pb, sys_aclqos_entry_t* p_entry)
+{
+	CTC_PTR_VALID_CHECK(p_entry);
+
+	//remove all sub entries from tcam and db
+	CTC_ERROR_RETURN(
+		_sys_humber_aclqos_remove_sub_entry(0, p_entry->p_label, p_entry);
+	
+	CTC_ERROR_RETURN(
+		_sys_humber_aclqos_remove_entry_from_db(pb, p_entry));
+	
+	return CTC_E_NONE;	
+}
+
+
+int32
+sys_humber_aclqos_entry_insert(uint32 label_id, ctc_aclqos_label_type_t label_type, uint32 entry_id, ctc_aclqos_entry_t *p_ctc_entry)
+{
+	sys_aclqos_entry_t *p_sys_entry[CTC_MAX_LOCAL_CHIP_NUM];
+	sys_aclqos_action_t *p_sys_action;
+	sys_aclqos_key_t *p_sys_key;
+	sys_aclqos_label_t *p_label = NULL;
+	uint8 lchip, lchip_num;
+	int32 ret;
+	uint8 is_service_label = 0;
+	uint16 block_index = 0;
+	sys_aclqos_block_t *pb = NULL;
+	uint8 asic_type;
+	uint16 shift_amount = 0;
+
+	CTC_PTR_VALID_CHECK(p_ctc_entry);
+	CTC_MIN_VALUE_CHECK(p_ctc_entry->entry_id, 1);	
+
+	if(CTC_SERVICE_LABEL == label_type)
+	{
+		is_service_label = 1;
+	}
+
+	CTC_ERROE_RETURN(sys_humber_aclqos_label_lookup(label_id, is_service_laebl, &p_label));
+	if(!p_label)
+	{
+		return CTC_E_ACLQOS_LABEL_NOT_EXIST;
+	}
+
+	if(CTC_QOS_LABEL == label_type)
+	{
+		if(IS_ACL_LABEL(p_label->type))
+			return CTC_E_ACLQOS_DIFFERENT_TYPE;
+	}else if(CTC_ACL_LABEL == label_type)
+	{	
+		if(IS_QOS_LABEL(p_label->type))
+			return CTC_E_ACLQOS_DIFFERENT_TYPE;				
+	}
+
+	kal_memset(p_sys_entry, 0, sizeof(p_sys_entry));
+	asic_type = acl_master->asic_type[p_ctc_entry->key.type];
+	pb = &acl_master->block[asic_type];
+	
+	//add entry
+	lchip_num = sys_humber_get_local_chip_num();
+	for(lchip = 0, lchip < lchip_num; lchip++)
+	{
+		if(!p_label->p_label[lchip])
+		{
+			continue;
+		}
+		_sys_humber_acl_get_sys_entry_by_eid(p_ctc_entry->entry_id, &p_sys_entry[lchip]);
+		
+		if(p_sys_entry[lchip])
+		{
+			ret = CTC_E_ACLQOS_ENTRY_EXIST;
+			p_sys_entry[lchip] = NULL;
+			goto ERR;
+		}
+		
+		p_sys_entry[lchip] = (sys_aclqos_entry_t *)mem_malloc(MEM_ACLQOS_MODELE, sizeof(sys_aclqos_entry_t));
+		if(!p_sys_entry[lchip])
+		{
+			ret = CTC_E_NO_MEMORY;
+			goto ERR;
+		}
+
+		kal_memset(p_sys_entry[lchip], 0, sizeof(sys_aclqos_entry_t));
+		p_sys_entry[lchip]->entry_id = p_ctc_entry->entry_id;
+		p_sys_action = &p_sys_entry[lchip]->action;
+		p_sys_key = &p_sys_entry[lchip]->key;
+	
+		p_sys_entry[lchip]->p_label = p_label;
+	
+		//map acl/qos action	
+		ret = _sys_humber_aclqos_map_action(lchip, p_label, &p_ctc_entry->action, p_sys_action);
+		if(ret)
+		{
+			goto ERR;
+		}
+
+		//map acl/qos key	
+		ret = _sys_humber_aclqos_map_key(lchip, p_label, &p_ctc_entry->key, p_sys_key);
+		if(ret)
+		{
+			goto ERR;
+		}
+
+		//get entry offset	
+		ret = _sys_humber_acl_get_block_index(pb, entry_id, &block_index, &shift_amount);
+		if(ret)
+		{
+			goto ERR;
+		}
+		p_sys_entry[lchip]->block_index = block_index;
+	
+		//write entries
+		ret = _sys_humber_aclqos_entry_write(pb, p_sys_entry[lchip]);
+		if(ret)
+		{
+			goto ERR;
+		}
+
+		if(DO_REORDER(shift_amount, pb->entry_count))
+		{
+			_sys_humber_acl_reorder(pb, pb->entry_count - 1, 0);
+		}
+	}
+	return CTC_E_NONE;
+ERR:
+	lchip_num = lchip + 1;
+	for(lchip = 0; lchip < lchip_num; lchip++)
+	{
+		if(!p_label->p_index[lchip])
+		{
+			continue;
+		}
+		if(p_sys_entry[lchip])
+		{
+			_sys_humber_aclqos_entry_remove(pb, p_sys_entry[lchip]);
+		}
+	}
+	
+	return ret;
+}		
+		
+
+
